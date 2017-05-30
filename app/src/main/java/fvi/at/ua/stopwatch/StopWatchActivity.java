85e3f5da4a14087e6701eebe460a0f9873b07ca1
Victoria Frantsuh <eboko1@gmail.com>
2017-05-30 22:43:07 +0300

    Log.i("bundle","seved")

--- app/src/main/java/fvi/at/ua/stopwatch/StopWatchActivity.java
+++ app/src/main/java/fvi/at/ua/stopwatch/StopWatchActivity.java
@@ -1,97 +1,84 @@
 package fvi.at.ua.stopwatch;
 
+import android.app.Activity;
 import android.os.Handler;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.test.RenamingDelegatingContext;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class StopWatchActivity extends AppCompatActivity {
-    private  int seconds = 0;
+public class StopWatchActivity extends Activity {
+    private int seconds = 0;
     private boolean running = false;
-    private  boolean wasRunning;
+    private boolean wasRunning;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
 
-
-        Button start = (Button)findViewById(R.id.start_button);
-        Button stop = (Button)findViewById(R.id.stop_button);
-        Button reset = (Button)findViewById(R.id.reset_button);
-
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
             wasRunning = savedInstanceState.getBoolean("wasRunning");
+            Log.i("BUNDLE","!null");
+        } else {
+            Log.i("BUNDLE","null");
         }
         runTimer();
     }
-
-    public  void  onSaveInstanceState(Bundle savedInstanceState){
-        savedInstanceState.putInt("second", seconds );
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("second", seconds);
         savedInstanceState.putBoolean("running", running);
-        savedInstanceState.putBoolean("wasRunning", wasRunning );
-
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+        Log.i("BUNDLE","saved "+ seconds+" running " +running );
     }
-
-    protected void onStop(){
-        super.onStop();
+    @Override
+    protected void onPause() {
+        super.onPause();
         wasRunning = running;
         running = false;
     }
-
-    protected void onStart(){
-         super.onStart();
-        if(wasRunning){
-            running  =true;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
         }
     }
 
 
-    public void onClickStart(View view){
+    public void onClickStart(View view) {
         running = true;
     }
 
-    public void onClickStop(View view){
-        super.onStop();
-        wasRunning = running;
+    public void onClickStop(View view) {
         running = false;
     }
 
-    public void onClickReset(View view){
+    public void onClickReset(View view) {
         running = false;
         seconds = 0;
     }
 
-    private void runTimer(){
-
-        final  TextView time_view = (TextView)findViewById(R.id.time_view);
+    private void runTimer() {
+        final TextView time_view = (TextView) findViewById(R.id.time_view);
         final Handler handler = new Handler();
-        handler.post(new Runnable(){
-
+        handler.post(new Runnable() {
             @Override
             public void run() {
                 int hour = seconds / 3600;
                 int minute = (seconds % 3600) / 60;
                 int sec = seconds % 60;
-
                 String time = String.format("%d:%02d:%02d", hour, minute, sec);
                 time_view.setText(time);
-                if (running){
+                if (running) {
                     seconds++;
                 }
-                handler.postDelayed(this,1000);
+                handler.postDelayed(this, 1000);
             }
         });
     }
-
-
-
-
-
 }
